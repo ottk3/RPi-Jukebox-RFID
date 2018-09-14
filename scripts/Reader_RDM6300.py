2b48576c6b88ed6ef585f41874c1c1a9908c50f1
Sven Rojek <sven@rojek.de>
2018-09-14 22:51:03 +0200

    Only allow retriggering of UUIDs that are in self.retrigger_list

--- scripts/Reader_RDM6300.py
+++ scripts/Reader_RDM6300.py
@@ -24,6 +24,7 @@ mv Reader.py Reader_default.py; mv Reader_RDM6300.py Reader.py
 import serial
 import string
 import atexit
+from datetime import datetime, timedelta
 
 
 class Reader:
@@ -32,6 +33,9 @@ class Reader:
         baudrate = 9600
         ser_timeout = 0.1
         self.last_card_id = ''
+        self.retrigger_list = []    # UUIDs in this list are allowed for multi-triggering
+        self.retrigger_interval = 1500  # minimal interval for re-triggering the same UUID in milliseconds
+        self.next_trigger = datetime.now() + timedelta(milliseconds=self.retrigger_interval)
         atexit.register(self.cleanup)
         try:
             self.rfid_serial = serial.Serial(device, baudrate, timeout=ser_timeout)
@@ -47,8 +51,8 @@ class Reader:
                 try:
                     read_byte = self.rfid_serial.read()
 
-                    if read_byte == b'\x02':    # start byte
-                        while read_byte != b'\x03':     # end bye
+                    if read_byte == b'\x02':  # start byte
+                        while read_byte != b'\x03':  # end bye
                             read_byte = self.rfid_serial.read()
                             byte_card_id += read_byte
 
@@ -57,12 +61,16 @@ class Reader:
                         card_id = ''.join(x for x in card_id if x in string.printable)
 
                         # Only return UUIDs with correct length
-                        if len(card_id) == 12 and card_id != self.last_card_id:
-                            self.last_card_id = card_id
-                            self.rfid_serial.reset_input_buffer()
-                            return self.last_card_id
+                        if len(card_id) == 12 and \
+                            (card_id != self.last_card_id or  # An other UUID as last time
+                             # Same UUID but re-trigger interval is ok
+                             (card_id in self.retrigger_list and self.next_trigger < datetime.now())):
+                                self.next_trigger = datetime.now() + timedelta(milliseconds=self.retrigger_interval)
+                                self.last_card_id = card_id
+                                self.rfid_serial.reset_input_buffer()
+                                return self.last_card_id
 
-                        else:   # wrong UUID length or aleady send that UUID last time
+                        else:  # wrong UUID length or aleady send that UUID last time
                             self.rfid_serial.reset_input_buffer()
 
                 except ValueError as ve:
@@ -70,6 +78,14 @@ class Reader:
 
         except serial.SerialException as se:
             print(se)
+
+    def check_retrigger(self, uuid):
+        if uuid in self.retrigger_list and \
+                datetime.now() > self.next_trigger:
+                    datetime.now() + timedelta(milliseconds=self.retrigger_interval)
+                    return True
+        else:
+            return False
 
     def cleanup(self):
         self.rfid_serial.close()
